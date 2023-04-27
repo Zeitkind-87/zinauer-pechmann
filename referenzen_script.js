75eb2a4176272174280219c316a0b860f19ab2f2
Christian Weiß <54807547+cp-dev-87@users.noreply.github.com>
2023-04-27 22:38:59 +0200

    Add files via upload

--- referenzen_script.js
+++ referenzen_script.js
@@ -1,3 +1,10 @@
+// Preloader
+var loader = document.getElementById("preloader");
+
+window.addEventListener("load", function(){
+    loader.style.display = "none";
+});
+
 // Schließen des Dropdown-Menüs, wenn auf einen Link geklickt wird
 var dropdown = document.getElementsByClassName("dropdown");
 for (var i = 0; i < dropdown.length; i++) {
