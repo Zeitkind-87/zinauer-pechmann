409313dfab43191ec7ef56ec0c2183cf993399f6
Christian Weiß <54807547+cp-dev-87@users.noreply.github.com>
2023-04-27 21:12:37 +0200

    Add files via upload

---
+++ index_script.js
@@ -0,0 +1,18 @@
+// Schließen des Dropdown-Menüs, wenn auf einen Link geklickt wird
+var dropdown = document.getElementsByClassName("dropdown");
+for (var i = 0; i < dropdown.length; i++) {
+dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.getElementsByClassName("container-dropdown")[0];
+    if (dropdownContent.style.display === "block") {
+    dropdownContent.style.display = "none";
+    } else {
+    dropdownContent.style.display = "block";
+    }            
+});
+}
+
+var dropbtn = document.getElementById("myBtn");
+dropbtn.addEventListener("click", function() {
+this.classList.toggle("clicked");
+});   
